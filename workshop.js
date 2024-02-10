b8060c455d742e230be7d2b4c29cf51d550ec616
John Perkins <mrgreen@MrGreens-Laptop.local>
2024-02-10 12:19:13 -0600

    Revised

--- workshop.js
+++ workshop.js
@@ -1,77 +1,79 @@
-const freeLancers = [
-    {
-        lancerName: "Alice",
-        occupation: "Writer",
-        startingPrice: 30  
-    },
-    {
-        lancerName: "Bob",
-        occupation: "Teacher",
-        startingPrice: 50  
-    },
-    {
-        lancerName: "Carol",
-        occupation: "Programmer",
-        startingPrice: 70  
-    },
-  ];
+// STATE
 
-  const maxAmount = 1
+const freelancersNotDisplayedYet = [
+  { name: "Alice", price: 30, occupation: "writer" },
+  { name: "Bob", price: 50, occupation: "teacher" },
+  { name: "Carol", price: 70, occupation: "programmer" },
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
 
-  let title = document.getElementById("title")
-  title.innerText = "Freelance Forum"
+const freelancersToDisplay = []
 
-  let averagePrice = document.getElementById("averagePrice")
-  averagePrice.innerText = "The Average Starting Price is $50"
+const addFreelancerInterval = setInterval(addFreelancer, 2000)
 
-  for(i=0; i < freeLancers.length; i++){
+function render(){
+  // document.querySelector to find the container to put rows in
+  const freelancerContainer = document.querySelector("#freelancers")
 
-  let name = document.createElement("tr")
-  name.innerHTML = `<td>${freeLancers[i].lancerName}</td><td>${freeLancers[i].occupation}</td><td>${freeLancers[i].startingPrice}</td>`
-  document.getElementById("tbody").append(name)
+  // .map to turn freelancer objects into row elements
+  const freelancerElements = freelancersToDisplay.map(freelancer=> {
+      // freelancer looks like
+      // { name: "Dr. Slice", price: 25, occupation: "gardener" }
+      // if you forget: console.log("freelancer", freelancer)
+      const rowElement = document.createElement("tr")
+
+      const nameTd = document.createElement("td")
+      nameTd.innerText = freelancer.name
+
+      const occupationTd = document.createElement("td")
+      occupationTd.innerText = freelancer.occupation
+
+      const priceTd = document.createElement("td")
+      priceTd.innerText = freelancer.price
+
+      rowElement.append(nameTd, occupationTd, priceTd)
+      return rowElement
+  })
+
+  // .replaceChildren(... ) to update the HTML
+  freelancerContainer.replaceChildren(...freelancerElements)
+
+  const averageElement =document.querySelector("#avg-msg")
+  const averagePrice = calculateAveragePrice(freelancersToDisplay)
+  const averagePriceRounded = averagePrice.toFixed(2)
+  const message = `The average starting price is $${averagePriceRounded}`
+  averageElement.innerText = message
+}
+
+function calculateAveragePrice(freelancers) {
+  let count = freelancers.length
+  // 1. set result variable to "nothing"
+  let sum = 0
+  // 2. for loop over input array
+  for (const freelancer of freelancers){
+  // freelancer looks like
+  // { name: "Dr. Slice", price: 25, occupation: "gardener" }  
+      const price = freelancer.price  
+  // 3. update result variable with current item in array
+      sum = sum + price
   }
+  // 4. return result variable 
+  const averagePrice = sum/count
+  return averagePrice
+}
 
+function addFreelancer() {
+  const nextFreelancer = freelancersNotDisplayedYet.shift()
+  freelancersToDisplay.push(nextFreelancer)
 
-  const newFreelancers = [
-    { name: "Dr. Slice", price: 25, occupation: "Gardener" },
-    { name: "Dr. Pressure", price: 51, occupation: "Programmer" },
-    { name: "Prof. Possibility", price: 43, occupation: "Teacher" },
-    { name: "Prof. Prism", price: 81, occupation: "Teacher" },
-    { name: "Dr. Impulse", price: 43, occupation: "Teacher" },
-    { name: "Prof. Spark", price: 76, occupation: "Programmer" },
-    { name: "Dr. Wire", price: 47, occupation: "Teacher" },
-    { name: "Prof. Goose", price: 72, occupation: "Driver" },
-  ];
-
-  const addInfoIntervalId = setInterval(addInfo, 2000)
-  
   render()
-
-  function addInfo(){
-    for(i=0; i < newFreelancers.length; i++){
-
-    let newName = document.createElement("tr")
-    newName.innerHTML = `<td>${newFreelancers[i].name}</td><td>${newFreelancers[i].occupation}</td><td>${newFreelancers[i].price}</td>`
-    document.getElementById("tbody").append(newName)
-    }
-
-    render()
-
-    if(newFreelancers.length >= maxAmount){
-        clearInterval(addInfoIntervalId)
-    }
-  }
-
-  
-
-//   const addFreeLancerInterval = setInterval(addFreeLancer, 2000)
+}
 
 
-// function addFreeLancer(newFreelancer)
-
-// i = 0
-// setInterval() => {
-    
-//     addFreeLancer( freelancers[i])
-//     i++
-// },2000
